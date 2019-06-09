59b28eaac7af2248994ab90ada50eed32cc6d5b2
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2019-06-09 09:41:56 +0100

    Updated utils.js.

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,5 @@
 import checkPropTypes from 'check-prop-types';
 
-export const findByTestAttr = (component, attr) => {
-    return component.find(`[data-test='${attr}']`);
-}
+export const findByTestAttr = (component, attr) => (component.find(`[data-test='${attr}']`));
 
-export const checkProps = (component, expectedProps) => {
-    return checkPropTypes(component.propTypes, expectedProps, 'props', component.name);
-}
+export const checkProps = (component, expectedProps) => (checkPropTypes(component.propTypes, expectedProps, 'props', component.name));
